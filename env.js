fc444d10cdb3c2d2bd9880072573a93c17ec50db
Michael Watts <hello@michaelwatts.co>
2018-08-01 13:01:25 +0100

    add dotenv module and environment variables

---
+++ env.js
@@ -0,0 +1,6 @@
+// require dotenv in order to use these environment variables
+require('dotenv').config();
+
+// these are our API key vars
+console.log(process.env.GUARDIAN);
+console.log(process.env.NYTIMES);
---
+++ test/tests.js
@@ -0,0 +1,14 @@
+const tape = require('tape');
+require('dotenv').config();
+
+tape('check tape is working', (t) => {
+  const expected = 2;
+  t.equal(1 + 1, expected, '2 should equal 2');
+  t.end();
+});
+
+tape('check environment vars are set', (t) => {
+  t.equal((process.env.GUARDIAN === 'undefined'), false, 'Guardian API key should not be undefined');
+  t.equal((process.env.NYTIMES === 'undefined'), false, 'NY Times API key should not be undefined');
+  t.end();
+})
