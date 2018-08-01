4951abdcff2b9d39a2fcdaebe54b2f5b48f2a0a6
Jessie Beech <jessie@jessiebeech.com>
2018-08-01 16:07:43 +0100

    refactor render function

--- public/logic.js
+++ public/logic.js
@@ -4,21 +4,33 @@ let submitButton = document.getElementById('submit');
 let newsDiv1 = document.getElementById('news-1');
 let newsDiv2 = document.getElementById('news-2');
 // FOR TESTING BEFORE APIS ARE READY
-var exampleParsedObj = {
-    Guardian: {
+var exampleResponse = {
+    Guardian: [{
         heading: "A news article about something relevant",
         author: "Jimmy J Jackson",
         date: "XX-XX-XXXX",
         img_url: "url...",
         link_url: "url..."
-    },
-    NYTimes: {
+    },{
+        heading: "A second article about something mentioning the query",
+        author: "Kate Beard",
+        date: "XX-XX-XXXX",
+        img_url: "url...",
+        link_url: "url..."
+    }],
+    NYTimes: [{
         heading: "A news article about something relevant",
-        author: "Jimmy J Jackson",
+        author: "Jessie Beech",
         date: "XX-XX-XXXX",
         img_url: "url...",
         link_url: "url..."  
-    }
+    },{
+        heading: "A second article about something mentioning the query",
+        author: "Second Author",
+        date: "XX-XX-XXXX",
+        img_url: "url...",
+        link_url: "url..."
+    }]
 }
 
 // FUNCTION TO GRAB INPUT FROM SEARCH FIELD
@@ -28,9 +40,8 @@ submitButton.addEventListener('click', function(event){
     let searchTerm = document.getElementById('input').value;
     console.log(searchTerm);
     // Makes XHR with query and invokes render as callback
-    // guardian XHR
-    //genericXHR(searchTerm,renderXHRCallback);
-    renderXHRCallback(exampleParsedObj);
+    genericXHR(searchTerm,renderXHRCallback);
+    // renderXHRCallback(response);
 });
 
 // GENERIC XHR
@@ -41,7 +52,7 @@ function genericXHR(query, cb) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var parsedObj = JSON.parse(xhr.responseText);
-            return cb(parsedObj,dest);
+            return cb(parsedObj);
         } else if (xhr.readyState == 4 && xhr.status !== 200) {
             console.log("Sorry, XHR error");
         }
@@ -53,64 +64,54 @@ function genericXHR(query, cb) {
 // RENDER CALLBACK FROM XHR
 
 function renderXHRCallback(response) {
-    for (var key in exampleParsedObj) {
+    for (var key in response) {
         if (key == "Guardian") {
-            // Heading
-            var heading = document.createElement('h2')
-            newsDiv1.appendChild(heading)
-            var headingContent = document.createTextNode(exampleParsedObj[key].heading)
-            heading.appendChild(headingContent);
-            // Author
-            var author = document.createElement('h3')
-            newsDiv1.appendChild(author)
-            var authorContent = document.createTextNode(exampleParsedObj[key].author)
-            author.appendChild(authorContent);
-            // Date
-            var date = document.createElement('h3')
-            newsDiv1.appendChild(date)
-            var dateContent = document.createTextNode(exampleParsedObj[key].date)
-            date.appendChild(dateContent);
-            // Image 
-            var img = document.createElement('img');
-            img.setAttribute('src', exampleParsedObj[key].img_url);
-            newsDiv1.appendChild(img)
-            // Link to webpage
-            var link = document.createElement('a');
-            link.setAttribute('href', exampleParsedObj[key].link_url);
-            link.setAttribute('target', "_blank");
-            link.textContent = "See full UK article";
-            newsDiv1.appendChild(link);
+            // Heading, Author & Date
+            response[key].forEach( function(article) {
+                appendTextElement(newsDiv1,'h2',article.heading)
+                appendTextElement(newsDiv1,'h3',article.author)
+                appendTextElement(newsDiv1,'h3',article.date)
+                // Image 
+                appendImage(newsDiv1,article.img_url,"article image")
+                // Link 
+                appendLink(newsDiv1,article.link_url,"See full article")
+            });
         } else if (key == "NYTimes") {
-            // Heading
-            var heading = document.createElement('h2')
-            newsDiv2.appendChild(heading)
-            var headingContent = document.createTextNode(exampleParsedObj[key].heading)
-            heading.appendChild(headingContent);
-            // Author
-            var author = document.createElement('h3')
-            newsDiv2.appendChild(author)
-            var authorContent = document.createTextNode(exampleParsedObj[key].author)
-            author.appendChild(authorContent);
-            // Date
-            var date = document.createElement('h3')
-            newsDiv2.appendChild(date)
-            var dateContent = document.createTextNode(exampleParsedObj[key].date)
-            date.appendChild(dateContent);
-            // Image 
-            var img = document.createElement('img');
-            img.setAttribute('src', exampleParsedObj[key].img_url);
-            newsDiv2.appendChild(img)
-            // Link to webpage
-            var link = document.createElement('a');
-            link.setAttribute('href', exampleParsedObj[key].link_url);
-            link.setAttribute('target', "_blank");
-            link.textContent = "See full US article";
-            newsDiv2.appendChild(link);
+            // Heading, Author & Date
+            response[key].forEach( function(article) {
+                appendTextElement(newsDiv2,'h2',article.heading)
+                appendTextElement(newsDiv2,'h3',article.author)
+                appendTextElement(newsDiv2,'h3',article.date)
+                // Image 
+                appendImage(newsDiv2,article.img_url,"article image")
+                // Link 
+                appendLink(newsDiv2,article.link_url,"See full article")
+            });
         }
     }
 };
 
 
+function appendTextElement(parent,elementType,elementTextContent) {
+    var newElement = document.createElement(elementType)
+    newElement.textContent = elementTextContent
+    parent.appendChild(newElement)
+}
+
+function appendImage(parent,src,alt) {
+    var newElement = document.createElement('img')
+    newElement.setAttribute('src', src);
+    newElement.setAttribute('alt', alt);
+    parent.appendChild(newElement)
+}
+
+function appendLink(parent,href,linkText) {
+    var link = document.createElement('a');
+    link.setAttribute('href', href);
+    link.setAttribute('target', "_blank");
+    link.textContent = linkText;
+    parent.appendChild(link);
+}
 
 /* <h2 class="">A news article about something relevant</h2>
 <h3>Author: Jimmy</h3>
