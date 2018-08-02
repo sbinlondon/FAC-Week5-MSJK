d6b9d5474f0e2e33d78f22c08db0d6d86b27704c
Jessie Beech <jessie@jessiebeech.com>
2018-08-02 12:47:37 +0100

    set up requests and parser functions

--- src/parser.js
+++ src/parser.js
@@ -1,37 +1,42 @@
-/* 
-API REQUEST FORMATS
 
-GUARDIAN
+const responseNumber = 3;
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "c49aae7f6fec4b2996bd9cd4231ad8f2",
-  'q': QUERYSTRING,
-  'sort': "newest",
-  'fl': "headline, web_url, pub_date, multimedia",
-  'page': 0
-});
+const parseGuardian = (response) => {
+  const data = JSON.parse(response);
+  const resultObj = { Guardian: [] };
+  for (let i = 0; i < responseNumber; i += 1) {
+    const article = {};
+    article.heading = data.response.results[i].fields.headline;
+    article.author = data.response.results[i].tags.webTitle;
+    article.date = data.response.results[i].webPublicationDate; //("2018-08-01T08:00:08Z")
+    article.img_url = data.response.results[i].fields.thumbnail;
+    article.link_url = data.response.results[i].fields.shortUrl;
+    resultObj.Guardian.push(article);
+  }
+  // console.log(resultObj);
+  return resultObj;
+}
 
-NYT 
+const parseNYTimes = (response) => {
+  const dataNY = JSON.parse(response);
+  const resultObj = { NYTimes: [] };
+  for (let i = 0; i < responseNumber; i += 1) {
+    const article = {};
+    article.heading = dataNY.response.docs[i].headline.main;
+    article.author = dataNY.response.docs[i].byline.original;
+    article.date = dataNY.response.docs[i].pub_date;
+    if (dataNY.response.docs[i].multimedia.length > 0) {
+      let link = "https://www.nytimes.com/";
+      link += dataNY.response.docs[i].multimedia[0].url;
+      article.img_url = link;
+    } else {
+      article.img_url = "https://placeimg.com/640/480/arch";
+    }
+    article.link_url = dataNY.response.docs[i].web_url;
+    resultObj.NYTimes.push(article);
+  }
+  // console.log(resultObj);
+  return resultObj;
+};
 
-https://content.guardianapis.com/search?q=" + QUERYSTRING + "&format=json&from-date=2010-01-01&page-size=5&show-tags=contributor&show-fields=headline,thumbnail,short-url,byline&order-by=newest&order-date=published&api-key=dc8546af-0f12-4efe-9f34-eaba3153ae96
-
-API RESPONSE FORMATS
-
-GUARDIAN
-
-Headline: response.results[i].fields.headline
-Author: response.results[i].tags.webTitle
-Date: response.results[i].webPublicationDate ("2018-08-01T08:00:08Z")
-Image: response.results[i].fields.thumbnail
-Link: response.results[i].fields.shortUrl
-
-NYT
-
-Headline: response.docs[i].headline.main
-Author: response.docs[i].web_url.byline.original ("By FIRST LAST")
-Date: response.docs[i].pub_date
-Image: response.docs[i].multimedia[17].url
-Link: response.docs[i].web_url
-
-*/
+module.exports = { parseGuardian, parseNYTimes };
