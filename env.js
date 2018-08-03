adf279300cfd9614c94eebed858d68c03b0bbd92
Michael Watts <hello@michaelwatts.co>
2018-08-03 12:29:36 +0100

    remove redundant env.js file

--- env.js
+++
@@ -1,6 +0,0 @@
-// require dotenv in order to use these environment variables
-require('dotenv').config();
-
-// these are our API key vars
-console.log(process.env.GUARDIAN);
-console.log(process.env.NYTIMES);
