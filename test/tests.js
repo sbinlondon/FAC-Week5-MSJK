c52498177b492b3f3610e586d0fa1076d136f2c9
Michael Watts <hello@michaelwatts.co>
2018-08-03 12:30:01 +0100

    remove env.js from tests

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,5 @@
 const tape = require('tape');
 require('dotenv').config();
-//const envJS = require('../env.js');
 
 tape('check tape is working', (t) => {
   const expected = 2;
@@ -13,3 +12,4 @@ tape('check environment vars are set', (t) => {
   t.equal((process.env.NYTIMES === undefined), false, 'NY Times API key should not be undefined');
   t.end();
 })
+;
